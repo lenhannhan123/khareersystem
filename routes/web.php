8a11fc4df9079a8178677689e41f484d4fb781f0
Lê Nguyễn Thành Nhân <74607205+lenhannhan123@users.noreply.github.com>
2022-09-19 00:59:22 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -15,32 +15,38 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/svit', function () {
-    return view('svit.svitindex');
-});
 
-Route::get('/khareersharing/english', function () {
-    return view('sharing.sharing-learningenlish');
-});
 
-Route::get('/khareersharing/it', function () {
-    return view('sharing.sharing-learningit');
-});
 
-Route::get('/khareersharing/content', function () {
-    return view('sharing.sharing-learningcontent');
-});
 
-Route::get('/khareersharing/photo', function () {
-    return view('sharing.sharing-learningphoto');
-});
-
-Route::get('/', function () {
-    return view('khareer.index');
-});
-Route::get('/home', function () {
-    return view('khareer.index');
-});
-Route::get('/trang_chu', function () {
-    return view('khareer.index');
+Route::middleware([link::class])->group(function () {
+    Route::get('/svit', function () {
+        return view('svit.svitindex');
+    });
+    
+    Route::get('/khareersharing/english', function () {
+        return view('sharing.sharing-learningenlish');
+    });
+    
+    Route::get('/khareersharing/it', function () {
+        return view('sharing.sharing-learningit');
+    });
+    
+    Route::get('/khareersharing/content', function () {
+        return view('sharing.sharing-learningcontent');
+    });
+    
+    Route::get('/khareersharing/photo', function () {
+        return view('sharing.sharing-learningphoto');
+    });
+    
+    Route::get('/', function () {
+        return view('khareer.index');
+    });
+    Route::get('/home', function () {
+        return view('khareer.index');
+    });
+    Route::get('/trang_chu', function () {
+        return view('khareer.index');
+    });
 });
